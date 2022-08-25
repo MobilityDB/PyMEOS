cd6e0d9c77a356f95364c313fd1ee317e7c0417d
Diviloper <vdiviloper@gmail.com>
2022-08-25 14:40:26 +0200

    Update Docker to finally be able to create manylinux images

--- pymeos_cffi/docker/build_wheels.sh
+++ pymeos_cffi/docker/build_wheels.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e -u -x
-
+#
+# Command: docker run --rm -ti -v <path-to-pymeos-project>:/PyMEOS -v <path-to-store-wheels>:/wheelhouse pymeos/builder
+#
 function repair_wheel {
   wheel="$1"
-  if ! auditwheel -V show "$wheel"; then
+  if ! auditwheel -v show "$wheel"; then
     echo "Skipping non-platform wheel $wheel"
   else
-    auditwheel -V repair "$wheel" -w /wheelhouse/
+    auditwheel -v repair "$wheel" -w /wheelhouse/
   fi
 }
 
@@ -16,7 +18,6 @@ for PYBIN in /opt/python/*/bin; do
   "${PYBIN}/pip" install -r /PyMEOS/pymeos_cffi/dev-requirements.txt
   #    "${PYBIN}/pip" wheel /PyMEOS/pymeos_cffi/ -w /wheelhouse_int/
   "${PYBIN}/pip" wheel /PyMEOS/pymeos_cffi/ --no-deps -w /wheelhouse_int/
-  break
 done
 
 echo "==============REPAIR=============="
@@ -29,8 +30,5 @@ echo "==============TEST=============="
 # Install and test package
 for PYBIN in /opt/python/*/bin; do
   "${PYBIN}/pip" install pymeos_cffi -f /wheelhouse
-  "${PYBIN}/python"
-  break
+  "${PYBIN}/python" -c "from pymeos_cffi import meos_initialize, meos_finish; meos_initialize(); meos_finish()"
 done
-
-bash
