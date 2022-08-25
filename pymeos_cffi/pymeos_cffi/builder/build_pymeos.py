14d6fb70ced09c3e7cc4f61ecd7bc3c3c28a6979
Diviloper <vdiviloper@gmail.com>
2022-08-25 13:27:21 +0200

    Change include in ffi compilation

--- pymeos_cffi/pymeos_cffi/builder/build_pymeos.py
+++ pymeos_cffi/pymeos_cffi/builder/build_pymeos.py
@@ -4,10 +4,11 @@ ffibuilder = FFI()
 
 with open('./pymeos_cffi/builder/meos.h', 'r') as f:
     content = f.read()
+
 ffibuilder.cdef(content)
 
 ffibuilder.set_source('_meos_cffi',
-                      '#include "meos_mod.h"   // the C header of the library',
+                      '#include "meos.h"   // the C header of the library',
                       libraries=['meos'], )  # library name, for the linker
 
 if __name__ == "__main__":  # not when running with setuptools
