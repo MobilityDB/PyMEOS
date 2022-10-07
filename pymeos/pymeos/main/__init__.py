211bcdc6698c841aab32eb8365181de2d30e7753
Diviloper <vdiviloper@gmail.com>
2022-10-07 19:23:05 +0200

    Add TNumber to init

--- pymeos/pymeos/main/__init__.py
+++ pymeos/pymeos/main/__init__.py
@@ -1,12 +1,15 @@
 from .tbool import TBool, TBoolInst, TBoolSeq, TBoolSeqSet
 from .tfloat import TFloat, TFloatInst, TFloatSeq, TFloatSeqSet
 from .tint import TInt, TIntInst, TIntSeq, TIntSeqSet
+from .tnumber import TNumber
 from .tpoint import TPointInst, TPointSeq, TPointSeqSet, \
     TGeomPoint, TGeomPointInst, TGeomPointSeq, TGeomPointSeqSet, \
     TGeogPoint, TGeogPointInst, TGeogPointSeq, TGeogPointSeqSet
 from .ttext import TText, TTextInst, TTextSeq, TTextSeqSet
+
 __all__ = [
     'TBool', 'TBoolInst', 'TBoolSeq', 'TBoolSeqSet',
+    'TNumber',
     'TInt', 'TIntInst', 'TIntSeq', 'TIntSeqSet',
     'TFloat', 'TFloatInst', 'TFloatSeq', 'TFloatSeqSet',
     'TText', 'TTextInst', 'TTextSeq', 'TTextSeqSet',
