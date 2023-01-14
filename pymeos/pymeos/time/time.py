2c478700d087f225cf64e62e39a1d55d2787050c
Diviloper <vdiviloper@gmail.com>
2023-01-14 12:56:16 +0100

    Add documentation to Time

--- pymeos/pymeos/time/time.py
+++ pymeos/pymeos/time/time.py
@@ -6,3 +6,11 @@ from .timestampset import TimestampSet
 from datetime import datetime
 
 Time = Union[datetime, TimestampSet, Period, PeriodSet]
+"""
+Union type that includes all Time types in PyMEOS:  
+
+- :class:`datetime` for timestamps  
+- :class:`TimestampSet` for sets of timestamps  
+- :class:`Period` for periods of time  
+- :class:`PeriodSet` for sets of periods of time  
+"""
