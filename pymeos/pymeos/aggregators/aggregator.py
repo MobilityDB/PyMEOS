6a0ad9563620888b9587cf5f7d00b78efc18f46b
Diviloper <vdiviloper@gmail.com>
2023-02-16 14:51:17 +0100

    Add documentation for base aggregators

--- pymeos/pymeos/aggregators/aggregator.py
+++ pymeos/pymeos/aggregators/aggregator.py
@@ -13,31 +13,107 @@ from ..time import Time
 
 ResultType = TypeVar('ResultType', bound=Union[Temporal, Time, Box])
 SourceType = TypeVar('SourceType', bound=Union[Temporal, Time, Box])
+StateType = TypeVar('StateType')
+SourceMeosType = TypeVar('SourceMeosType')
+ResultMeosType = TypeVar('ResultMeosType')
 SelfAgg = TypeVar('SelfAgg', bound='Aggregation')
 
 
 class BaseAggregator(Generic[SourceType, ResultType], abc.ABC):
-    _add_function = None
-    _final_function = temporal_tagg_finalfn
+    """
+    Abstract class for all aggregations.
+
+    Can be easily extended overriding the :func:`~pymeos.aggregators.aggregator.BaseAggregator._add_function` and
+    :func:`~pymeos.aggregators.aggregator.BaseAggregator._final_function` when the aggregation uses the inner MEOS
+    objects, or :func:`~pymeos.aggregators.aggregator.BaseAggregator._add` and
+    :func:`~pymeos.aggregators.aggregator.BaseAggregator._finish` for arbitrary aggregations.
+    """
+
+    @staticmethod
+    def _add_function(state: Optional[StateType], meos_object: SourceMeosType) -> StateType:
+        """
+        Add `meos_object` to the aggregation. Usually a MEOS function.
+        Args:
+            state: current state of the aggregation.
+            meos_object: new MEOS object to aggregate.
+
+        Returns:
+            New state of the aggregation after adding `meos_object`.
+
+        """
+        raise NotImplemented
+
+    @staticmethod
+    def _final_function(state: StateType) -> ResultMeosType:
+        """
+        Return the current value of the aggregation. Usually a MEOS function.
+        Args:
+            state: current state of the aggregation.
+
+        Returns:
+            Value of the aggregation.
+
+        """
+        return temporal_tagg_finalfn(state)
+
+    @classmethod
+    def _add(cls, state: Optional[StateType], temporal: SourceType) -> StateType:
+        """
+        Add the `temporal` object to the aggregation.
+        Args:
+            state: current state of the aggregation.
+            temporal: new PyMEOS object to aggregate.
+
+        Returns:
+            New state of the aggregation after adding `temporal`.
+
+        """
+        return cls._add_function(state, temporal._inner)
+
+    @classmethod
+    def _finish(cls, state) -> ResultType:
+        """
+        Return the current value of the aggregation.
+        Args:
+            state: current state of the aggregation.
+
+        Returns:
+            Value of the aggregation.
+
+        """
+        result = cls._final_function(state)
+        return _TemporalFactory.create_temporal(result)
 
     @classmethod
     def aggregate(cls, temporals: List[SourceType]) -> ResultType:
+        """
+        Aggregate a list of PyMEOS object at once.
+
+        Useful when only the final result is desired and all the objects are available.
+
+        For aggregating in a streaming fashion, see
+        :func:'pymeos.aggregators.aggregator.BaseAggregator.start_aggregation'.
+
+        Args:
+            temporals: list of PyMEOS objects to aggregate.
+
+        Returns:
+            Result of applying the aggregation to the passed objects.
+
+        """
         state = None
         for t in temporals:
             state = cls._add(state, t)
         return cls._finish(state)
 
     @classmethod
-    def _add(cls, state, temporal: SourceType):
-        return cls._add_function(state, temporal._inner)
-
-    @classmethod
-    def _finish(cls, state) -> SourceType:
-        result = cls._final_function(state)
-        return _TemporalFactory.create_temporal(result)
-
-    @classmethod
     def start_aggregation(cls) -> Aggregation:
+        """
+        Return an :class:`Aggregation` object that holds the state of a new aggregation.
+
+        Returns:
+            An :class:`Aggregation` instance
+        """
         return Aggregation(cls._add, cls._finish)
 
     @classmethod
@@ -47,6 +123,12 @@ class BaseAggregator(Generic[SourceType, ResultType], abc.ABC):
 
 
 class Aggregation(Generic[SourceType, ResultType]):
+    """
+    Class representing an aggregation in process.
+
+    This class is returned by the :func:`~pymeos.aggregators.aggregator.BaseAggregator.start_aggregation` method
+    of :class:`BaseAggregator` subclasses, and shouldn't be created directly by the final user.
+    """
 
     def __init__(self, add_function, finish_function) -> None:
         super().__init__()
@@ -55,25 +137,92 @@ class Aggregation(Generic[SourceType, ResultType]):
         self._state = None
 
     def add(self: SelfAgg, new_temporal: SourceType) -> SelfAgg:
+        """
+        Add a new element to the aggregation.
+
+        Returns itself to allowing chaining
+
+        Examples:
+            >>> aggregation.add(temporal1).add(temporal2)
+            Is equivalent to
+            >>> aggregation.add(temporal1)
+            >>> aggregation.add(temporal2)
+
+        Args:
+            new_temporal: PyMEOS object to add to the aggregation
+
+        Returns:
+            `self`
+        """
         self._state = self._add_function(self._state, new_temporal)
         return self
 
     def aggregation(self) -> ResultType:
+        """
+        Return the current aggregation value.
+
+        Note that this doesn't finish the aggregation, so more elements can be still added.
+
+        Examples:
+            >>> aggregation.add(temporal1).add(temporal2)
+            >>> intermediate = aggregation.aggregation()
+            >>> aggregation.add(temporal3).add(temporal4)
+            >>> final_result = aggregation.aggregation()
+
+        Returns:
+            Result of the aggregation
+        """
         return self._finish_function(self._state)
 
 
 class BaseGranularAggregator(BaseAggregator[SourceType, ResultType]):
+    """
+    Abstract class for granular aggregations.
+    """
+
+    @staticmethod
+    def _add_function(state: Optional[StateType], meos_object: SourceMeosType, interval=None,
+                      origin=None) -> StateType:
+        raise NotImplemented
 
     @classmethod
-    def aggregate(cls, temporals: List[SourceType], interval: Optional[Union[str, timedelta]] = None,
-                  origin: Union['str', datetime] = '1970-01-01') -> ResultType:
+    def aggregate(cls, temporals: List[SourceType], interval=None, origin=None) -> ResultType:
+        """
+        Aggregate a list of PyMEOS object at once with certain granularity.
+
+        Useful when only the final result is desired and all the objects are available.
+
+        For aggregating in a streaming fashion, see
+        :func:`~pymeos.aggregators.aggregator.BaseGranularAggregator.start_aggregation`.
+
+        Args:
+            temporals: list of PyMEOS objects to aggregate.
+            interval: width of the aggregation intervals
+            origin: starting value of the first interval
+
+        Returns:
+            Result of applying the granular aggregation to the passed objects.
+
+        """
         state = None
         for t in temporals:
             state = cls._add(state, t, interval, origin)
         return cls._finish(state)
 
     @classmethod
-    def _add(cls, state, temporal: SourceType, interval=None, origin='1970-01-01'):
+    def _add(cls, state: Optional[StateType], temporal: SourceType, interval=None, origin=None) -> StateType:
+        """
+        Add the `temporal` object to the aggregation.
+        Args:
+            state: current state of the aggregation.
+            temporal: new PyMEOS object to aggregate.
+            interval: width of the aggregation intervals
+            origin: starting value of the first interval
+
+        Returns:
+            New state of the aggregation after adding `temporal`.
+
+        """
         interval_converted = timedelta_to_interval(interval) if isinstance(interval, timedelta) else \
             pg_interval_in(interval, -1) if isinstance(interval, str) else None
         origin_converted = datetime_to_timestamptz(origin) if isinstance(origin, datetime) else \
@@ -81,12 +230,18 @@ class BaseGranularAggregator(BaseAggregator[SourceType, ResultType]):
         return cls._add_function(state, temporal._inner, interval_converted, origin_converted)
 
     @classmethod
-    def start_aggregation(cls, interval: Optional[Union[str, timedelta]] = None,
-                          origin: Union['str', datetime] = '1970-01-01') -> GranularAggregation[SourceType, ResultType]:
+    def start_aggregation(cls, interval=None, origin=None) -> GranularAggregation[SourceType, ResultType]:
         return GranularAggregation(cls._add, cls._finish, interval, origin)
 
 
 class GranularAggregation(Aggregation[SourceType, ResultType]):
+    """
+    Class representing a granular aggregation in process.
+
+    This class is returned by the :func:`~pymeos.aggregators.aggregator.BaseGranularAggregator.start_aggregation` method
+    of :class:`BaseGranularAggregator` subclasses, and shouldn't be created directly by the final user.
+    """
+
     def __init__(self, add_function, finish_function, interval, origin) -> None:
         super().__init__(add_function, finish_function)
         self._interval = interval
