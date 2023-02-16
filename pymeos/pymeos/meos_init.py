60b2123c798a3b056dd62049d272589185388ef0
Diviloper <vdiviloper@gmail.com>
2023-02-16 10:43:40 +0100

    Add documentation for initialization and finalization functions

--- pymeos/pymeos/meos_init.py
+++ pymeos/pymeos/meos_init.py
@@ -3,9 +3,26 @@ from typing import Optional
 from pymeos_cffi import meos_initialize, meos_finish
 
 
-def pymeos_initialize(timezone: Optional[str] = None):
+def pymeos_initialize(timezone: Optional[str] = None) -> None:
+    """
+    Initializes the underlying MEOS platform.
+    Must be called before any other PyMEOS function.
+
+    Args:
+        timezone: :class:`str` indicating the desired timezone to be used. Defaults to system timezone.
+
+    MEOS Functions:
+        meos_initialize
+    """
     meos_initialize(timezone)
 
 
 def pymeos_finalize() -> None:
+    """
+    Cleans up the underlying MEOS platform.
+    Should be called at the end after PyMEOS is no longer used.
+
+    MEOS Functions:
+        meos_finish
+    """
     meos_finish()
