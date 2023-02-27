3284f8621667b20729b2e556dd0a38bd194358cc
Diviloper <vdiviloper@gmail.com>
2023-02-28 00:58:29 +0100

    Update Time type docstring

--- pymeos/pymeos/time/time.py
+++ pymeos/pymeos/time/time.py
@@ -9,8 +9,8 @@ Time = Union[datetime, TimestampSet, Period, PeriodSet]
 """
 Union type that includes all Time types in PyMEOS:  
 
-- :class:`datetime` for timestamps  
-- :class:`TimestampSet` for sets of timestamps  
-- :class:`Period` for periods of time  
-- :class:`PeriodSet` for sets of periods of time  
+- :class:`~datetime.datetime` for timestamps  
+- :class:`~pymeos.time.timestampset.TimestampSet` for sets of timestamps  
+- :class:`~pymeos.time.period.Period` for periods of time  
+- :class:`~pymeos.time.periodset.PeriodSet` for sets of periods of time  
 """
