36d947ebc5f593a9824f2ea0301466afed2fccdc
Diviloper <vdiviloper@gmail.com>
2023-05-14 16:50:21 +0200

    Add TInterpolation tests

---
+++ pymeos/tests/temporal/interpolation_test.py
@@ -0,0 +1,26 @@
+from pymeos import TInterpolation
+from tests.conftest import TestPyMEOS
+import pytest
+
+
+class TestTInterpolation(TestPyMEOS):
+
+    @pytest.mark.parametrize(
+        'source, result',
+        [
+            ('discrete', TInterpolation.DISCRETE),
+            ('linear', TInterpolation.LINEAR),
+            ('stepwise', TInterpolation.STEPWISE),
+            ('none', TInterpolation.NONE),
+        ],
+        ids=['period', 'periodset', 'timestamp', 'timestampset']
+    )
+    def test_from_string(self, source, result):
+        assert TInterpolation.from_string(source) == result
+
+    def test_from_string_invalid(self):
+        assert TInterpolation.from_string('invalid') == TInterpolation.NONE
+
+    def test_from_string_invalid_none(self):
+        with pytest.raises(ValueError):
+            TInterpolation.from_string('invalid', none=False)
