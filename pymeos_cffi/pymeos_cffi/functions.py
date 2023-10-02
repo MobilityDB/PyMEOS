c279bf3e734c3ccff2a9a13fc688a2d52063cd8b
Diviloper <vdiviloper@gmail.com>
2023-10-02 15:17:22 +0200

    Remove spans dependency and postgis support from pymeos_cffi

--- pymeos_cffi/pymeos_cffi/functions.py
+++ pymeos_cffi/pymeos_cffi/functions.py
@@ -3,12 +3,10 @@ from typing import Any, Tuple, Optional, List, Union
 
 import _meos_cffi
 from .errors import raise_meos_exception
-import postgis as pg
 import shapely.geometry as spg
 from dateutil.parser import parse
-from shapely import wkt, wkb, get_srid
+from shapely import wkt, get_srid
 from shapely.geometry.base import BaseGeometry
-from spans.types import floatrange, intrange
 
 _ffi = _meos_cffi.ffi
 _lib = _meos_cffi.lib
@@ -63,39 +61,25 @@ def interval_to_timedelta(interval: Any) -> timedelta:
     return timedelta(days=interval.day, microseconds=interval.time)
 
 
-def geo_to_gserialized(geom: Union[pg.Geometry, BaseGeometry], geodetic: bool) -> 'GSERIALIZED *':
+def geo_to_gserialized(geom: BaseGeometry, geodetic: bool) -> 'GSERIALIZED *':
     if geodetic:
         return geography_to_gserialized(geom)
     else:
         return geometry_to_gserialized(geom)
 
 
-def geometry_to_gserialized(geom: Union[pg.Geometry, BaseGeometry]) -> 'GSERIALIZED *':
-    if isinstance(geom, pg.Geometry):
-        text = geom.wkt
-        # if geom.has_srid():
-        #     text = f'SRID={geom.srid};{text}'
-    elif isinstance(geom, BaseGeometry):
-        text = wkt.dumps(geom)
-        if get_srid(geom) > 0:
-            text = f'SRID={get_srid(geom)};{text}'
-    else:
-        raise TypeError('Parameter geom must be either a PostGIS Geometry or a Shapely BaseGeometry')
+def geometry_to_gserialized(geom: BaseGeometry) -> 'GSERIALIZED *':
+    text = wkt.dumps(geom)
+    if get_srid(geom) > 0:
+        text = f'SRID={get_srid(geom)};{text}'
     gs = pgis_geometry_in(text, -1)
     return gs
 
 
-def geography_to_gserialized(geom: Union[pg.Geometry, BaseGeometry]) -> 'GSERIALIZED *':
-    if isinstance(geom, pg.Geometry):
-        text = geom.wkt
-        # if geom.has_srid():
-        #     text = f'SRID={geom.srid};{text}'
-    elif isinstance(geom, BaseGeometry):
-        text = wkt.dumps(geom)
-        if get_srid(geom) > 0:
-            text = f'SRID={get_srid(geom)};{text}'
-    else:
-        raise TypeError('Parameter geom must be either a PostGIS Geometry or a Shapely BaseGeometry')
+def geography_to_gserialized(geom: BaseGeometry) -> 'GSERIALIZED *':
+    text = wkt.dumps(geom)
+    if get_srid(geom) > 0:
+        text = f'SRID={get_srid(geom)};{text}'
     gs = pgis_geography_in(text, -1)
     return gs
 
@@ -106,22 +90,6 @@ def gserialized_to_shapely_point(geom: 'const GSERIALIZED *', precision: int = 6
 
 def gserialized_to_shapely_geometry(geom: 'const GSERIALIZED *', precision: int = 6) -> BaseGeometry:
     return wkt.loads(gserialized_as_text(geom, precision))
-
-
-def intrange_to_intspan(irange: intrange) -> 'Span *':
-    return intspan_make(irange.lower, irange.upper, irange.lower_inc, irange.upper_inc)
-
-
-def intspan_to_intrange(ispan: 'Span *') -> intrange:
-    return intrange(intspan_lower(ispan), intspan_upper(ispan), ispan.lower_inc, ispan.upper_inc)
-
-
-def floatrange_to_floatspan(frange: floatrange) -> 'Span *':
-    return floatspan_make(frange.lower, frange.upper, frange.lower_inc, frange.upper_inc)
-
-
-def floatspan_to_floatrange(fspan: 'Span *') -> floatrange:
-    return floatrange(floatspan_lower(fspan), floatspan_upper(fspan), fspan.lower_inc, fspan.upper_inc)
 
 
 def as_tinstant(temporal: 'Temporal *') -> 'TInstant *':
@@ -135,11 +103,9 @@ def as_tsequence(temporal: 'Temporal *') -> 'TSequence *':
 def as_tsequenceset(temporal: 'Temporal *') -> 'TSequenceSet *':
     return _ffi.cast('TSequenceSet *', temporal)
 
-
 # -----------------------------------------------------------------------------
 # ----------------------End of manually-defined functions----------------------
 # -----------------------------------------------------------------------------
-
 def lwpoint_make(srid: 'int32_t', hasz: int, hasm: int, p: 'const POINT4D *') -> 'LWPOINT *':
     srid_converted = _ffi.cast('int32_t', srid)
     p_converted = _ffi.cast('const POINT4D *', p)
