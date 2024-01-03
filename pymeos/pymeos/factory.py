cfd262b64558408cfe4084acd724ca8d599180cd
Diviloper <vdiviloper@gmail.com>
2024-01-03 11:37:06 +0100

    Add new factory for Collection types

--- pymeos/pymeos/factory.py
+++ pymeos/pymeos/factory.py
@@ -20,13 +20,31 @@ from .main import (
     TGeogPointSeq,
     TGeogPointSeqSet,
 )
+from .collections import (
+    GeometrySet,
+    GeographySet,
+    IntSet,
+    IntSpan,
+    IntSpanSet,
+    FloatSet,
+    FloatSpan,
+    FloatSpanSet,
+    TextSet,
+    DateSet,
+    DateSpan,
+    DateSpanSet,
+    TsTzSet,
+    TsTzSpan,
+    TsTzSpanSet,
+)
 
 
 class _TemporalFactory:
     """
     Factory class to create the proper PyMEOS class from a MEOS object.
 
-    This class is used internally by PyMEOS classes and there shouldn't be any need to be used outside of them.
+    This class is used internally by PyMEOS classes and there shouldn't be any need to
+    be used outside of them.
     """
 
     _mapper = {
@@ -59,9 +77,60 @@ class _TemporalFactory:
             inner: MEOS object.
 
         Returns:
-            An appropriate subclass of :class:`Temporal` wrapping `inner`.
+            An instance of the appropriate subclass of :class:`Temporal` wrapping
+            `inner`.
         """
         if inner is None:
             return None
         temp_type = (inner.temptype, inner.subtype)
         return _TemporalFactory._mapper[temp_type](_inner=inner)
+
+
+class _CollectionFactory:
+    """
+    Factory class to create the proper PyMEOS collection class from a MEOS object.
+
+    This class is used internally by PyMEOS classes and there shouldn't be any need
+    to be used outside of them.
+    """
+
+    _mapper = {
+        MeosType.T_GEOMSET: GeometrySet,
+        MeosType.T_GEOGSET: GeographySet,
+        MeosType.T_INTSET: IntSet,
+        MeosType.T_INTSPAN: IntSpan,
+        MeosType.T_INTSPANSET: IntSpanSet,
+        MeosType.T_FLOATSET: FloatSet,
+        MeosType.T_FLOATSPAN: FloatSpan,
+        MeosType.T_FLOATSPANSET: FloatSpanSet,
+        MeosType.T_TEXTSET: TextSet,
+        MeosType.T_DATESET: DateSet,
+        MeosType.T_DATESPAN: DateSpan,
+        MeosType.T_DATESPANSET: DateSpanSet,
+        MeosType.T_TSTZSET: TsTzSet,
+        MeosType.T_TSTZSPAN: TsTzSpan,
+        MeosType.T_TSTZSPANSET: TsTzSpanSet,
+    }
+
+    @staticmethod
+    def create_collection(inner):
+        """
+        Creates the appropriate PyMEOS Collection class from a meos object.
+
+        Args:
+            inner: MEOS object.
+
+        Returns:
+            An instance of the appropriate subclass of :class:`Collection` wrapping
+            `inner`.
+        """
+        if inner is None:
+            return None
+
+        attributes = ["spansettype", "spantype", "settype"]
+        collection_type = next(
+            getattr(inner, attribute)
+            for attribute in attributes
+            if hasattr(inner, attribute)
+        )
+        return _CollectionFactory._mapper[collection_type](_inner=inner)
