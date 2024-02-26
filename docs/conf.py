3d02b3ae726e69f98755bbcf7b4bd2652c178f30
Diviloper <vdiviloper@gmail.com>
2024-02-26 21:27:01 +0100

    Update docs version

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'PyMEOS'
 copyright = '2023, Víctor Diví'
 author = 'Víctor Diví'
-release = '1.1.3.rc1'
+release = '1.1.3.rc2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
