2605a955ba505c2688b99eef43083a5f12a4667d
diviloper <vdiviloper@gmail.com>
2024-08-12 13:49:38 +0200

    Update modified functions

--- pymeos/temporal/tinstant.py
+++ pymeos/temporal/tinstant.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from abc import ABC
 from datetime import datetime
-from typing import Optional, Union, Any, TYPE_CHECKING, TypeVar, List
+from typing import Optional, Union, TypeVar, List
 
 from pymeos_cffi import *
 
